93245ebc3487f70505d18a412f95ef50a8872bb8
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-01 15:20:40 +0300

    basic jsx and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 function App() {
-  return   
+  return (
+    <div className="calc-container">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <div className="span">
+        <button>C</button>
+        <button>DEL</button>
+      </div>
+      <div className="span">
+      <button>+</button>
+      <button>−</button>
+      <button>×</button>
+      <button>÷</button>
+      
+      </div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+
+      <div className="span">
+        <button>0</button>
+        <button>,</button>
+        <button>=</button>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
