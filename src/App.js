fc027969943f0042f4eaac2e795f59b8bdc3b7cf
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-12 11:26:49 +0300

    fixing reducer, changing buttons into components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
+/* eslint-disable default-case */
 import { useReducer } from "react";
+import DigitButton from "./components/DigitButton";
+import OperationButton from "./components/OperationButton"
 
-function reducer() {
-  
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  CHOOSE_OPERATOR: "choose-operator",
+  EQUALS: "equals",
+};
+
+function reducer(state, { type, payload }) {  // action => type
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") {return state};
+      if (payload.digit === "." && state.currentOperand.includes(".")) {return state};
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      }
+  }
 }
 
 function App() {
-
-  const [state, dispatch] = useReducer(reducer)
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
 
   return (
     <div className="calc-container">
       <div className="output span">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <div className="span">
         <button>C</button>
         <button>DEL</button>
       </div>
       <div className="span">
-      <button>+</button>
-      <button>−</button>
-      <button>×</button>
-      <button>÷</button>
-      
+      <OperationButton operation="+" dispatch={dispatch} />
+      <OperationButton operation="−" dispatch={dispatch} />
+      <OperationButton operation="×" dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
       </div>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
 
       <div className="span">
-        <button>,</button>
-        <button>0</button>
+      <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
         <button>=</button>
-        
       </div>
     </div>
   );
