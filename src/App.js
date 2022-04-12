b79dfa59c5b2fd0cb1f92fe350bac0e3e303cfa2
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-12 11:47:46 +0300

    calculations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable default-case */
 import { useReducer } from "react";
 import DigitButton from "./components/DigitButton";
-import OperationButton from "./components/OperationButton"
+import OperationButton from "./components/OperationButton";
 
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
@@ -11,16 +11,74 @@ export const ACTIONS = {
   EQUALS: "equals",
 };
 
-function reducer(state, { type, payload }) {  // action => type
+function reducer(state, { type, payload }) {
+  // action => type
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if (payload.digit === "0" && state.currentOperand === "0") {return state};
-      if (payload.digit === "." && state.currentOperand.includes(".")) {return state};
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+    case ACTIONS.CHOOSE_OPERATOR: {
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+        // if currently no numbers or an operator is already chosen,
+        //... do not accept operators
       }
+      if (state.currentOperand == null) {
+        
+      }
+
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,          
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+          operation: payload.operation,
+        };
+        //pass current state into previous operand, then clear current operand
+      }
+    }
+    return {
+      ...state,
+      previousOperand: evaluate(state),
+      currentOperand: null,
+      operation: payload.operation,
+    }
+    case ACTIONS.CLEAR:
+      return {};
   }
+}
+
+//as default, calculate current calculation before adding more numbers =>
+const evaluate = ({currentOperand, previousOperand, operation }) => {
+const current = parseFloat(currentOperand); 
+const previous = parseFloat(previousOperand); //convert strings into numbers
+if (isNaN(previous) || isNaN(current)) return ""
+//if not a number, return empty string (= do not calculate)
+let calculation = ""
+switch (operation) {
+  case "+":
+    calculation = previous + current;
+    break;
+  case "−":
+    calculation = previous - current;
+    break;
+  case "×":
+    calculation = previous * current;
+    break;
+  case "÷":
+    calculation = previous / current;
+    break;
+}
+return calculation.toString(); //convert calculation into string, and return it
 }
 
 function App() {
@@ -38,14 +96,14 @@ function App() {
         <div className="current-operand">{currentOperand}</div>
       </div>
       <div className="span">
-        <button>C</button>
+        <button onClick={() => dispatch({ type: ACTIONS.CLEAR })}>C</button>
         <button>DEL</button>
       </div>
       <div className="span">
-      <OperationButton operation="+" dispatch={dispatch} />
-      <OperationButton operation="−" dispatch={dispatch} />
-      <OperationButton operation="×" dispatch={dispatch} />
-      <OperationButton operation="÷" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <OperationButton operation="−" dispatch={dispatch} />
+        <OperationButton operation="×" dispatch={dispatch} />
+        <OperationButton operation="÷" dispatch={dispatch} />
       </div>
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -58,7 +116,7 @@ function App() {
       <DigitButton digit="9" dispatch={dispatch} />
 
       <div className="span">
-      <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
         <button>=</button>
       </div>
